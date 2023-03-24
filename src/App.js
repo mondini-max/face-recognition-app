f3aee8738021720054d8ea499fd7f1bb0d241498
Maxime Mondini <mh-mondini@hotmail.com>
2023-03-24 16:32:46 -0400

    add particles-bg

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Logo from './Components/Logo/Logo';
 import Navigation from './Components/Navigation/Navigation';
 import Rank from './Components/Rank/Rank';
 import SearchImageForm from './Components/SearchImageForm/SearchImageForm';
+import ParticlesBg from 'particles-bg';
 
 function App() {
   return (
     <div className='App'>
+      <ParticlesBg num={200} type='thick' bg={true} />
       <Navigation />
       <Logo />
       <Rank />
