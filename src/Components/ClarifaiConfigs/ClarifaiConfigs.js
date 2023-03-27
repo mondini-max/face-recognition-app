522e4f6ef69224b5cafd05d251b1c0fd5b748071
Maxime Mondini <mh-mondini@hotmail.com>
2023-03-27 18:15:34 -0400

    Refactor codes

---
+++ src/Components/ClarifaiConfigs/ClarifaiConfigs.js
@@ -0,0 +1,56 @@
+const {
+  REACT_APP_CLARIFAI_PAT,
+  // REACT_APP_CLARIFAI_APIKey,
+  REACT_APP_CLARIFAI_USER_ID,
+} = process.env;
+
+// face recognition model
+
+export const MODEL_ID = 'face-detection';
+export const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+
+// color detection model
+// const MODEL_ID = 'color-recognition';
+// const MODEL_VERSION_ID = 'dd9458324b4b45c2be1a7ba84d27cd04';
+
+export const clarifaiRequestOptionsConfig = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = REACT_APP_CLARIFAI_PAT;
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = REACT_APP_CLARIFAI_USER_ID;
+  const APP_ID = 'face-recoginition-app';
+  // Change these to whatever model and image URL you want to use
+  const IMAGE_URL = imageUrl;
+
+  ///////////////////////////////////////////////////////////////////////////////////
+  // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+  ///////////////////////////////////////////////////////////////////////////////////
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Key ' + PAT,
+    },
+    body: raw,
+  };
+
+  return requestOptions;
+};
