884b2d85afbf3aa130e8f7fdeb19b805c94bcfeb
Maxime Mondini <mh-mondini@hotmail.com>
2023-03-27 19:12:41 -0400

    creating signin and register forms

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Signin = ({ onRouteChange }) => {
+const Register = ({ onRouteChange }) => {
   return (
     <div className='br3 ba white b--white mv4 w-100 w-50-m w-25-1 mw6 center bg-black bw1'>
       <main className='pa4 f3 white-80'>
         <form className='measure tc'>
           <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
-            <legend className='f2 fw6 ph0 mh0'>Sign In</legend>
+            <legend className='f2 fw6 ph0 mh0'>Register</legend>
             <div className='mt3'>
               <label className='db fw6 lh-copy f4' for='email-address'>
                 Email
@@ -16,6 +16,17 @@ const Signin = ({ onRouteChange }) => {
                 type='email'
                 name='email-address'
                 id='email-address'
+              />
+            </div>
+            <div className='mt3'>
+              <label className='db fw6 lh-copy f4' for='fullName'>
+                Name
+              </label>
+              <input
+                className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 white'
+                type='text'
+                name='fullName'
+                id='fullName'
               />
             </div>
             <div className='mv3'>
@@ -35,15 +46,15 @@ const Signin = ({ onRouteChange }) => {
               onClick={() => onRouteChange('home')}
               className='b ph3 pv2 input-reset ba b--white bg-transparent grow pointer f4 dib white'
               type='submit'
-              value='Sign in'
+              value='Register'
             />
           </div>
           <div className='lh-copy mt3 f4'>
             <p
-              onClick={() => onRouteChange('register')}
+              onClick={() => onRouteChange('signin')}
               className=' link dim white db f6 pointer'
             >
-              Register
+              Sign In
             </p>
           </div>
         </form>
@@ -52,4 +63,4 @@ const Signin = ({ onRouteChange }) => {
   );
 };
 
-export default Signin;
+export default Register;
