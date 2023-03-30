834b06250519a081c90f2a0b6868686fc5bde9c2
Maxime Mondini <mh-mondini@hotmail.com>
2023-03-30 16:54:45 -0400

    add cors in sever

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { v4 as uuidv4 } from 'uuid';
-import bcrypt, { hash } from 'bcrypt';
+import bcrypt from 'bcrypt';
+import cors from 'cors';
 
 const app = express();
 const port = 4000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 const saltRounds = 11;
 
