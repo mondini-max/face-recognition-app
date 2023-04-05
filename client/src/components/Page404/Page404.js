d9adc33a14bf661e9b986c6b2e7267405ca02266
Maxime Mondini <mh-mondini@hotmail.com>
2023-04-05 16:23:11 -0400

    create 404 page

---
+++ client/src/components/Page404/Page404.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Page404 = () => {
+  return (
+    <section className='vh-100  bg-transparent'>
+      <header className='tc ph5 lh-copy'>
+        <h1 className='f1 f-headline-l code mb3 fw9 dib tracked-tight light-green '>
+          404
+        </h1>
+        <h2 className='tc f1-l fw1 light-green'>
+          Sorry, we can't find the page you are looking for.
+        </h2>
+      </header>
+    </section>
+  );
+};
