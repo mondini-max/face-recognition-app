105e593a611215f9c2539490790c156ee9ca9837
Maxime Mondini <mh-mondini@hotmail.com>
2023-04-05 16:56:45 -0400

    change redirect route

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -61,7 +61,7 @@ const Register = () => {
             setEmail('');
             setPassword('');
             setName('');
-            navigate('/home');
+            navigate('/');
             return;
           }
         })
