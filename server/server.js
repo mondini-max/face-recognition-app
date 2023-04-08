b1c51883a0ab3524541afbaedd9e0609c7c70717
Maxime Mondini <mh-mondini@hotmail.com>
2023-04-08 18:53:49 -0400

    create new route to update user info

--- server/server.js
+++ server/server.js
@@ -20,15 +20,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// app.get('/', (req, res) => {
-//   res.send(database.users);
-// });
+app.get('/', (req, res) => {
+  db.select('*')
+    .from('users')
+    .then((users) => res.json(users));
+});
 
 app.post('/signing', (req, res, next) =>
   signinController(req, res, next, db, bcrypt)
 );
 app.post('/register', (req, res) => registerController(req, res, db, bcrypt));
 app.get('/profile/:id', (req, res) => getUserById(req, res, db));
+app.put('/profile/:id', (req, res) => {
+  const { age, pet, name } = req.body.userInfo;
+  const { id } = req.params;
+  db('users')
+    .where({ id: id })
+    .update({ name: name, pet: pet, age })
+    .then((resp) => {
+      console.log('this is res in server', { resp });
+      if (resp) {
+        res.status(200).json('user info updated');
+      } else {
+        res.status(400).json('unable to update user');
+      }
+    })
+    .catch((err) => {
+      res.status(400).json('error updating user');
+    });
+});
 app.put('/image', (req, res) => updateRank(req, res, db));
 app.post('/smart-ai', (req, res) => smart_ai_controller(req, res));
 
